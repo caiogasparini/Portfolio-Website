db9ed69c2c49da71c4aac8619d54a9ea0cdf8e3d
Caio Gasparini <caio1057@gmail.com>
2021-06-01 20:11:52 -0300

    Create init.js

---
+++ init.js
@@ -0,0 +1,9 @@
+// inicializador materialize
+M.AutoInit();
+
+// load de item clicado
+window.onload = () => {
+    document.getElementById('li-item-0').style.backgroundColor = '#424242';
+    document.getElementById('pag-portfolium').style.display = 'flex';
+    document.getElementById('pag-hoIam').style.display = 'none';
+}
